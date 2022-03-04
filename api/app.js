aa4092c22a222609b181b50ea19124dafb401ce8
Ali Azlan <aliazlan2002@gmail.com>
2022-03-04 20:37:52 +0500

    entry file

---
+++ api/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./src/config/db')
+const colors = require('colors')
+const morgan = require('morgan')
+const cors = require('cors');
+
+dotenv.config({ path: './src/config/config.env'})
+
+connectDB()
+
+const authRoute = require('./src/routes/auth')
+const stdRoute = require('./src/routes/std')
+const ttrRoute = require('./src/routes/ttr')
+
+const app = express()
+app.use(express.json())
+app.use(cors());
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.get('/', (req, res, next) => res.status(200).json({ message: "Server running"} ))
+
+app.use('/api/v1/user', authRoute)
+app.use('/api/v1/std', stdRoute)
+app.use('/api/v1/ttr', ttrRoute)
+
+app.listen(process.env.PORT, () =>
+    console.log(`Server ready at http://localhost:${process.env.PORT}`)
+)
