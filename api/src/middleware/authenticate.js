c6c466632b73627568ca674ad39a28d834db3958
Ali Azlan <aliazlan2002@gmail.com>
2022-03-07 15:23:46 +0500

    added middleware for admin and modifief user middleware

--- api/src/middleware/authenticate.js
+++ api/src/middleware/authenticate.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const Admin = require('../models/admin')
+const Banned = require('../models/banned')
 
 module.exports = {
     isAuthenticated: (req, res, next) => {
@@ -9,13 +11,46 @@ module.exports = {
                     message: 'Unauthorized Access'
                 })
             }
+
+            Banned.findOne({user: decode.id})
+            .exec()
+            .then(ban => {
+                if(ban?._id){
+                    return res.status(401).json({
+                        message: 'Unauthorized Access'
+                    })
+                }
+
+                User.findById(decode.id)
+                .exec()
+                .then(user => {
+                    if(user._id != null){
+                        req.id = decode.id
+                        return next()
+                    }else{
+                        return res.status(401).json({
+                            message: 'Unauthorized Access'
+                        })
+                    }
+                })
+            })
+        })
+    },
+
+    isAdminAuth: (req, res, next) => {
+        jwt.verify(req.headers.token, process.env.SECRET_KEY, (error, decode) => {
+            if(error){
+                return res.status(401).json({
+                    message: 'Unauthorized Access'
+                })
+            }
             
-            User.findById(decode.id)
+            Admin.findById(decode.id)
             .exec()
             .then(user => {
                 if(user._id != null){
                     req.id = decode.id
-                    next()
+                    return next()
                 }else{
                     return res.status(401).json({
                         message: 'Unauthorized Access'
